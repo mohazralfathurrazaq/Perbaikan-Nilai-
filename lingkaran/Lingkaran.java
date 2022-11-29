8f0372a0b10587091911fe4968ef0b23cbb4b8c6
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-11-29 14:58:02 +0700

    Add files via upload

---
+++ lingkaran/Lingkaran.java
@@ -0,0 +1,14 @@
+
+package lingkaran;
+
+// Moh Azral Fathurrazaq
+
+
+public interface Lingkaran {
+    int PANJANG = 12;
+    int LEBAR = 7;
+    
+    void hitungLuas();
+    void tampilkanLuas();
+    
+}
---
+++ lingkaran/LuasSetengah.java
@@ -0,0 +1,23 @@
+
+package lingkaran;
+
+// Moh Azral Fathurrazaq
+// TI21E
+// 20210040135
+
+public class LuasSetengah {
+    private Lingkaran lingkaran;
+    
+    public LuasSetengah(Lingkaran lingkaran){
+        this.lingkaran = lingkaran;
+    }
+    
+    public void hitung(){
+        this.lingkaran.hitungLuas();
+    }
+    
+    public void tampilkan(){
+        this.lingkaran.tampilkanLuas();
+    }
+    
+}
